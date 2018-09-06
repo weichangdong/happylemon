c9978bcd294b26c48d1ad42ae93b9ee2b092d3e6
weichangdong <irogisi@126.com>
2018-09-06 10:30:09 +0800

    hapy

--- models/baseModel.go
+++ models/baseModel.go
@@ -24,11 +24,11 @@ func InitMysqlPool(conf conf.Conf) {
 	maxIdleConns := conf.Mysql.MaxIdleConns
 
 	var dbConfig = map[string]interface{}{
-		"Default":         "mysql_j20",  // 默认数据库配置
-		"SetMaxOpenConns": maxOpenConns, // (连接池)最大打开的连接数，默认值为0表示不限制
-		"SetMaxIdleConns": maxIdleConns, // (连接池)闲置的连接数, 默认1
+		"Default":         "happy_lemon", // 默认数据库配置
+		"SetMaxOpenConns": maxOpenConns,  // (连接池)最大打开的连接数，默认值为0表示不限制
+		"SetMaxIdleConns": maxIdleConns,  // (连接池)闲置的连接数, 默认1
 		"Connections": map[string]map[string]string{
-			"mysql_j20": map[string]string{ // 定义名为 mysql_dev 的数据库配置
+			"happy_lemon": map[string]string{ // 定义名为 mysql_dev 的数据库配置
 				"host":     host,      // 数据库地址
 				"username": username,  // 数据库用户名
 				"password": password,  // 数据库密码
